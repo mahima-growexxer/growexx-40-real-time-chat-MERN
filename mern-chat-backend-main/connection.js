94f6543daf80842aee5691658405a8a6fe8ebb9c
Mahima Gajiwala <mahima.gajiwala@growexx.com>
2022-09-14 09:32:52 +0530

    Initial commit

---
+++ mern-chat-backend-main/connection.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+// mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PW}@cluster0.s11qz.mongodb.net/chatAppMern?retryWrites=true&w=majority`, ()=> {
+//   console.log('connected to mongodb')
+// })
+
+mongoose.connect(`mongodb://localhost:27017/chatAppMern`, () => {
+    console.log('connected to mongodb')
+})
